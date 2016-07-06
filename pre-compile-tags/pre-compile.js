4b0df62949ebde94662c951606615b00e37724bd
Yasunari Fujieda <edamame1224@gmail.com>
2016-07-06 14:39:49 +0900

    remove compiled javascript

--- pre-compile-tags/pre-compile.js
+++
@@ -1,4 +0,0 @@
-riot.tag2('pre-compile', '<h3>{str}</h3> <hr>', 'pre-compile h3,[riot-tag="pre-compile"] h3,[data-is="pre-compile"] h3{ color: #00f; font-size: 150%; }', '', function(opts) {
-var str = 'Pre-Compile';
-this.str = str;
-});
