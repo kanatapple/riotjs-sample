19b4596858e73f35f469916ae4613ec5651492f0
Yasunari Fujieda <edamame1224@gmail.com>
2016-07-06 13:10:56 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# RiotJS Quick Start
+
+# Dependencies
+
+ - `node` and `npm`
+
+# Quick start
+
+```sh
+# install the dependencies with npm
+npm install
+
+# build and start the server
+npm start
+```
+
---
+++ pre-compile-tags/pre-compile.js
@@ -0,0 +1,4 @@
+riot.tag2('pre-compile', '<h3>{str}</h3> <hr>', 'pre-compile h3,[riot-tag="pre-compile"] h3,[data-is="pre-compile"] h3{ color: #00f; font-size: 150%; }', '', function(opts) {
+var str = 'Pre-Compile';
+this.str = str;
+});
